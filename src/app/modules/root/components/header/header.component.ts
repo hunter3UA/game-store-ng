dc46eb9a4c3039683e5c462aa9970ada3843ad6c
Oleksandr Solontsov <solontsov04s@gmail.com>
2022-05-23 11:30:24 +0300

    Fix totalGames

--- src/app/modules/root/components/header/header.component.ts
+++ src/app/modules/root/components/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { GameService } from 'src/app/modules/shared/services/game/game.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  constructor() {}
+  public totalCountOfGames: number;
+
+  constructor(private gameService: GameService) {
+    this.loadAllGames();
+  }
+
+  loadAllGames() {
+    this.gameService.getAllGames().subscribe((data) => {
+      this.totalCountOfGames = data.length;
+      console.log(data);
+    });
+  }
 }
